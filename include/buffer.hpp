9165935839f59555cebf38a45a73b34093cc54b0
Jamie Quinn <jjquinn42@gmail.com>
2017-07-17 13:58:53 +0100

    Added triangle filling & tests

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "vertex.hpp"
+#include <cassert>
 
 template <class T>
 class Buffer {
@@ -7,11 +9,17 @@ class Buffer {
     T get(int i, int j);
     Buffer(int _width, int _height);
     ~Buffer();
+    void fillTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData);
+    void fillLine(int x1, int x2, int y, T fillData);
+    void fillAll(T fillData);
   private:
     int width; // in pixels
     int height; // in pixels
     T* buffer;
     Buffer();
+
+    void fillBottomFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData);
+    void fillTopFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData);
 };
 
 template <class T>
@@ -28,13 +36,96 @@ Buffer<T>::~Buffer() {
 
 template <class T>
 void Buffer<T>::set(int i, int j, T item) {
-  // TODO Is bounds check necessary?
-  if ((i < width and i >= 0) and (j < height and j >= 0)) {
-    buffer[j*width + i] = item;
-  }
+  assert((i < width and i >= 0) and (j < height and j >= 0));
+  buffer[j*width + i] = item;
 }
 
 template <class T>
 T Buffer<T>::get(int i, int j) {
   return buffer[j*width + i];
+}
+
+template <class T>
+void Buffer<T>::fillLine(int x1, int x2, int y, T fillData) {
+  for(int x=x1; x<=x2; ++x) {
+    this->set(x, y, fillData);
+  }
+}
+
+template <class T>
+void Buffer<T>::fillTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData) {
+  // Assume vertices in clockwise order & sorted in y dir (i.e. v1.y <= v2.y <= v3.y)
+  assert(v1.y <= v2.y);
+  assert(v2.y <= v3.y);
+
+  /* check for trivial case of bottom-flat triangle */
+  if (v2.y == v3.y)
+  {
+    fillBottomFlatTriangle(v1, v2, v3, fillData);
+  }
+  /* check for trivial case of top-flat triangle */
+  else if (v1.y == v2.y)
+  {
+    fillTopFlatTriangle(v1, v2, v3, fillData);
+  }
+  else
+  {
+    /* general case - split the triangle in a topflat and bottom-flat one */
+    Vertex v4((int)(v1.x + ((real)(v2.y - v1.y) / (real)(v3.y - v1.y)) * (v3.x - v1.x)), v2.y);
+    if(v4.x < v2.x) {
+      fillBottomFlatTriangle(v1, v2, v4, fillData);
+      fillTopFlatTriangle(v4, v2, v3, fillData);
+    } else {
+      fillBottomFlatTriangle(v1, v4, v2, fillData);
+      fillTopFlatTriangle(v2, v4, v3, fillData);
+    }
+  }
+}
+
+template <class T>
+void Buffer<T>::fillBottomFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData) {
+  assert(v2.y == v3.y);
+  assert(v1.y < v2.y);
+
+  real invslope1 = (v2.x - v1.x) / (v2.y - v1.y);
+  real invslope2 = (v3.x - v1.x) / (v3.y - v1.y);
+
+  real curx1 = v1.x;
+  real curx2 = v1.x;
+
+  for (int scanlineY = v1.y; scanlineY <= v2.y; scanlineY++)
+  {
+    fillLine((int)curx2, (int)curx1, scanlineY, fillData);
+    curx1 += invslope1;
+    curx2 += invslope2;
+  }
+}
+
+template <class T>
+void Buffer<T>::fillTopFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData)
+{
+  assert(v1.y == v2.y);
+  assert(v1.y < v3.y);
+
+  real invslope1 = (v3.x - v1.x) / (v3.y - v1.y);
+  real invslope2 = (v3.x - v2.x) / (v3.y - v2.y);
+
+  real curx1 = v3.x;
+  real curx2 = v3.x;
+
+  for (int scanlineY = v3.y; scanlineY > v1.y; scanlineY--)
+  {
+    fillLine((int)curx1, (int)curx2, scanlineY, fillData);
+    curx1 -= invslope1;
+    curx2 -= invslope2;
+  }
+}
+
+template <class T>
+void Buffer<T>::fillAll(T fillData) {
+  for(int i=0; i<width; ++i) {
+    for(int j=0; j<height; ++j) {
+      set(i, j, fillData);
+    }
+  }
 }
--- test/buffer.cpp
+++ test/buffer.cpp
@@ -6,3 +6,112 @@ TEST_CASE("Buffer set and get works", "[buffer]") {
   buffer.set(1, 9, 10);
   REQUIRE( buffer.get(1, 9) == 10 );
 }
+
+TEST_CASE("fillLine works", "[buffer]") {
+  Buffer<int> buffer (10, 10);
+  buffer.fillAll(0);
+  buffer.fillLine(1, 5, 6, 1);
+  for(int i=0; i<10; ++i) {
+    for(int j=0; j<10; ++j) {
+      if(j==6 and i>=1 and i<=5) {
+        REQUIRE(buffer.get(i, j) == 1);
+      } else {
+        REQUIRE(buffer.get(i, j) == 0);
+      }
+    }
+  }
+}
+
+TEST_CASE("fillAll works", "[buffer]") {
+  Buffer<int> buffer (10, 10);
+  buffer.fillAll(0);
+  for(int i=0; i<10; ++i) {
+    for(int j=0; j<10; ++j) {
+      REQUIRE(buffer.get(i, j) == 0);
+    }
+  }
+}
+
+TEST_CASE("fillBottomFlatTriangle works", "[buffer]") {
+  int solution[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+
+  Buffer<int> buffer (10, 10);
+  buffer.fillAll(0);
+  Vertex v1(5, 1);
+  Vertex v2(7, 8);
+  Vertex v3(3, 8);
+  buffer.fillTriangle(v1, v2, v3, 1);
+
+  for(int j=0; j<10; ++j) {
+    for(int i=0; i<10; ++i) {
+      REQUIRE(buffer.get(i, j) == solution[j*10+i]);
+    }
+  }
+}
+
+TEST_CASE("fillTopFlatTriangle works", "[buffer]") {
+  int solution[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+
+  Buffer<int> buffer (10, 10);
+  buffer.fillAll(0);
+  Vertex v1(3, 1);
+  Vertex v2(7, 1);
+  Vertex v3(5, 8);
+  buffer.fillTriangle(v1, v2, v3, 1);
+
+  for(int j=0; j<10; ++j) {
+    for(int i=0; i<10; ++i) {
+      REQUIRE(buffer.get(i, j) == solution[j*10+i]);
+    }
+  }
+}
+
+TEST_CASE("fillTriangle works", "[buffer]") {
+  int solution[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 1, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+
+  Buffer<int> buffer (10, 10);
+  buffer.fillAll(0);
+  Vertex v1(6, 3);
+  Vertex v2(8, 5);
+  Vertex v3(5, 8);
+  buffer.fillTriangle(v1, v2, v3, 1);
+
+  for(int j=0; j<10; ++j) {
+    for(int i=0; i<10; ++i) {
+      REQUIRE(buffer.get(i, j) == solution[j*10+i]);
+    }
+  }
+}
