9deae1b740dafec8c7e44bd793a94f120aaec452
Jamie Quinn <jjquinn42@gmail.com>
2017-07-18 15:40:37 +0100

    Moved buffer rendering functionality to renderer & added tests. Z buffer working.

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -5,28 +5,36 @@
 template <class T>
 class Buffer {
   public:
-    void set(int i, int j, T);
-    T get(int i, int j);
+    void set(int i, int j, const T&);
+    T get(int i, int j) const;
     Buffer(int _width, int _height);
+    Buffer(int _width, int _height, T initial);
+    void setup(int _width, int _height);
     ~Buffer();
-    void fillTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData);
-    void fillLine(int x1, int x2, int y, T fillData);
     void fillAll(T fillData);
   protected:
-    int width; // in pixels
-    int height; // in pixels
+    int width, height; // in pixels
     T* buffer;
     Buffer();
-
-    void fillBottomFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData);
-    void fillTopFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData);
+    Buffer(const Buffer&);
 };
 
 template <class T>
 Buffer<T>::Buffer(int _width, int _height) {
+  setup(_width, _height);
+}
+
+template <class T>
+void Buffer<T>::setup(int _width, int _height) {
   buffer = new T[_width*_height];
   width = _width;
   height = _height;
+}
+
+template <class T>
+Buffer<T>::Buffer(int _width, int _height, T initial) {
+  setup(_width, _height);
+  fillAll(initial);
 }
 
 template <class T>
@@ -35,90 +43,14 @@ Buffer<T>::~Buffer() {
 }
 
 template <class T>
-void Buffer<T>::set(int i, int j, T item) {
+void Buffer<T>::set(int i, int j, const T& item) {
   assert((i < width and i >= 0) and (j < height and j >= 0));
   buffer[j*width + i] = item;
 }
 
 template <class T>
-T Buffer<T>::get(int i, int j) {
+T Buffer<T>::get(int i, int j) const {
   return buffer[j*width + i];
-}
-
-template <class T>
-void Buffer<T>::fillLine(int x1, int x2, int y, T fillData) {
-  for(int x=x1; x<=x2; ++x) {
-    this->set(x, y, fillData);
-  }
-}
-
-template <class T>
-void Buffer<T>::fillTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData) {
-  // Assume vertices in clockwise order & sorted in y dir (i.e. v1.y <= v2.y <= v3.y)
-  assert(v1.y <= v2.y);
-  assert(v2.y <= v3.y);
-
-  /* check for trivial case of bottom-flat triangle */
-  if (v2.y == v3.y)
-  {
-    fillBottomFlatTriangle(v1, v2, v3, fillData);
-  }
-  /* check for trivial case of top-flat triangle */
-  else if (v1.y == v2.y)
-  {
-    fillTopFlatTriangle(v1, v2, v3, fillData);
-  }
-  else
-  {
-    /* general case - split the triangle in a topflat and bottom-flat one */
-    Vertex v4((int)(v1.x + ((real)(v2.y - v1.y) / (real)(v3.y - v1.y)) * (v3.x - v1.x)), v2.y);
-    if(v4.x < v2.x) {
-      fillBottomFlatTriangle(v1, v2, v4, fillData);
-      fillTopFlatTriangle(v4, v2, v3, fillData);
-    } else {
-      fillBottomFlatTriangle(v1, v4, v2, fillData);
-      fillTopFlatTriangle(v2, v4, v3, fillData);
-    }
-  }
-}
-
-template <class T>
-void Buffer<T>::fillBottomFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData) {
-  assert(v2.y == v3.y);
-  assert(v1.y < v2.y);
-
-  real invslope1 = (v2.x - v1.x) / (v2.y - v1.y);
-  real invslope2 = (v3.x - v1.x) / (v3.y - v1.y);
-
-  real curx1 = v1.x;
-  real curx2 = v1.x;
-
-  for (int scanlineY = v1.y; scanlineY <= v2.y; scanlineY++)
-  {
-    fillLine((int)curx2, (int)curx1, scanlineY, fillData);
-    curx1 += invslope1;
-    curx2 += invslope2;
-  }
-}
-
-template <class T>
-void Buffer<T>::fillTopFlatTriangle(Vertex v1, Vertex v2, Vertex v3, T fillData)
-{
-  assert(v1.y == v2.y);
-  assert(v1.y < v3.y);
-
-  real invslope1 = (v3.x - v1.x) / (v3.y - v1.y);
-  real invslope2 = (v3.x - v2.x) / (v3.y - v2.y);
-
-  real curx1 = v3.x;
-  real curx2 = v3.x;
-
-  for (int scanlineY = v3.y; scanlineY > v1.y; scanlineY--)
-  {
-    fillLine((int)curx1, (int)curx2, scanlineY, fillData);
-    curx1 -= invslope1;
-    curx2 -= invslope2;
-  }
 }
 
 template <class T>
---
+++ include/renderer.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "precision.hpp"
+#include "buffer.hpp"
+#include "vertex.hpp"
+#include "triangle.hpp"
+
+class Renderer {
+  public:
+    Renderer(int _width, int _height);
+
+    void fillTriangle(Triangle& t);
+
+    real getZ(int i, int j) const;
+    Triangle* getTriangle(int i, int j) const;
+
+    friend std::ostream& operator<<(std::ostream&, const Renderer&);
+
+  private:
+    Buffer<real> zBuffer;
+    Buffer<Triangle*> itemBuffer;
+    int width, height;
+
+    void fillLine(int x1, int x2, int y, real z, Triangle& tri);
+    void fillBottomFlatTriangle(const Vertex& v1, const Vertex& v2, const Vertex& v3, Triangle& tri);
+    void fillTopFlatTriangle(const Vertex& v1, const Vertex& v2, const Vertex& v3, Triangle& tri);
+};
