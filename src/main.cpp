e5d0bd581d6fd23f991408b6faade61f29393c20
Jamie Quinn <jjquinn42@gmail.com>
2017-07-19 13:02:22 +0100

    Clear out main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,3 @@
-#include <iostream>
-
-#include "precision.hpp"
-#include "buffer.hpp"
-
-using namespace std;
-
-#define HEMICUBE_GRID_SIZE 10
-
-class Patch {
-};
-
-class Vertex {
-  public:
-    real x, y, z;
-};
-
 int main() {
-  Buffer<int> b(10, 10);
   return 0;
 }
