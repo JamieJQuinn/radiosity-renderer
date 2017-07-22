3bbf4c8b8bd2d1fe8d9cc7b2c276603e045c804e
Jamie Quinn <jjquinn42@gmail.com>
2017-07-22 18:10:20 +0100

    White colour is now white

--- include/colours.hpp
+++ include/colours.hpp
@@ -3,7 +3,7 @@
 #include "tgaimage.hpp"
 
 const TGAColor black  = TGAColor( 51, 51, 51, 255);
-const TGAColor white  = TGAColor(  0,  0,  0, 255);
+const TGAColor white  = TGAColor(255,255,255, 255);
 const TGAColor red    = TGAColor(199, 40, 40, 255);
 const TGAColor blue   = TGAColor( 24,120,120, 255);
 const TGAColor yellow = TGAColor(199,112, 40, 255);
