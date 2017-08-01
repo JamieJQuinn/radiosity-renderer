cf3bffa010fe14bcdeb7fdbc0eab60fff3626fc1
Jamie Quinn <jjquinn42@gmail.com>
2017-08-01 17:39:34 +0100

    Expose projection creation matrix

--- include/hemicube.hpp
+++ include/hemicube.hpp
@@ -7,3 +7,4 @@
 
 Matrix formHemicubeMVP(const Vec3f& eye, const Vec3f& dir, const Vec3f& up, int size);
 void calcFormFactorsFromModel(const Model& model, const int faceIdx, std::vector<float>& formFactors, int gridSize);
+Matrix formProjection(float l, float r, float b, float t, float n, float f);
