fa7c8feb589e57e6434c58b58fdf1797b83f32e3
Jamie Quinn <jjquinn42@gmail.com>
2017-08-02 11:14:05 +0100

    Fixed simple box render test

--- test/projection-test.cpp
+++ test/projection-test.cpp
@@ -51,14 +51,10 @@ TEST_CASE("Test moving the camera (filled triangles)", "[camera]") {
   Model model("test/simple_box.obj", "test/simple_box.mtl");
 
   Vec3f eye(2, 2.5, 3);
-  Vec3f centre(0,0,0);
+  Vec3f dir = eye*-1;
   Vec3f up(0, 0, 1);
-  Matrix modelView = lookAt(eye, centre, up);
-
-  Matrix P = Matrix::identity(4);
-  P.set(3, 2, -1.f/(eye-centre).norm());
-  Matrix V = viewportRelative(size/4, size/4, size/2, size/2);
-  Matrix MVP = V*P*modelView;
+  int size = 800;
+  Matrix MVP = formHemicubeMVP(eye, dir, up, size);
 
   Buffer<TGAColor> buffer(size, size, black);
 
